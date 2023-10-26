58f635cd883ad59138af8563ccae9470683dc651
Agustín Firpo <agustin_firpo@outlook.com.ar>
2023-10-26 01:49:47 -0300

    v4-w/json file

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,12 +1,12 @@
 import '../css/Testimonio.css';
 
-export const Testimonio = ({img, name, charge, testimony}) => {
+export const Testimonio = ({img, imgName, name, country, charge, company, testimony}) => {
   return (
     <div className='testimony-container'>
-      <img className='testimony-img' src={img}/>
+      <img className='testimony-img' src={img} alt={imgName}/>
         <div className='testimony__text-container'>
-          <p className='testimony__name'>{name}</p>
-          <p className='testimony__charge'>{charge}</p>
+          <p className='testimony__name'>{name} en <strong className='testimony__country'>{country}</strong></p>
+          <p className='testimony__charge'>{charge} en <strong className="testimony__company">{company}</strong></p>
           <p className='testimony__text'>{testimony}</p>
         </div>
     </div>
--- src/components/Testimonios.jsx
+++ src/components/Testimonios.jsx
@@ -3,27 +3,37 @@ import { Testimonio } from "./Testimonio";
 import emmaPhoto from "../images/testimonio-emma.png";
 import sarahPhoto from "../images/testimonio-sarah.png";
 import shawnPhoto from "../images/testimonio-shawn.png";
+import testimonios from '../json/testimonios.json';
 
 export const Testimonios = () => {
   return (
     <div className='testimonies-container'>
       <Testimonio
-      img={emmaPhoto}
-      name='Emma Bostian en Suecia'
-      charge='Ingeniera de Software en Spotify'
-      testimony='"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."'      
+        img={shawnPhoto}
+        imgName={testimonios.imgNames}
+        name={testimonios.names[0]}
+        country={testimonios.countries[0]}
+        charge={testimonios.charges[0]}
+        company={testimonios.companies[0]}
+        testimony={testimonios.testimonies[0]}      
       />
       <Testimonio
       img={sarahPhoto}
-      name='Sarah Chima en Nigeria'
-      charge='Ingeniera de Software en ChatDesk'
-      testimony='"freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble."'      
+      imgName={testimonios.imgNames[1]}
+      name={testimonios.names[1]}
+      country={testimonios.countries[1]}
+      charge={testimonios.charges[1]}
+      company={testimonios.companies[1]}
+      testimony= {testimonios.testimonies[1]}    
       />
       <Testimonio
-        img={shawnPhoto}
-        name='Shawn Wang en Singapur'
-        charge='Ingeniero de Software en Amazon'
-        testimony='"Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."'      
+      img={emmaPhoto}
+      imgName={testimonios.imgNames[2]}
+      name={testimonios.names[2]}
+      country={testimonios.countries[2]}
+      charge={testimonios.charges[1]}
+      company={testimonios.companies[2]}
+      testimony={testimonios.testimonies[2]}    
       />
     </div>
   )
